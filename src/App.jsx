d6602f646bffe7dccb4d1308e5175e8d44569811
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-04 00:55:33 +0500

    Adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ const App = () => {
   return (
     <>
      <body className=' bg-slate-900 grid place-content-center h-screen'>
+     {/* Creating Card for logo and Creator Name */}
+     
       <div className=" p-6 max-w-sm mx-auto bg-white rounded-lg flex items-center space-x-5 shadow-lg">
         <img className=" w-14 h-14" src={Logo} alt="" />
         <div className=" text-2xl font-medium text-black">
@@ -10,11 +12,15 @@ const App = () => {
          <p className=' text-slate-500 text-base'> By Aadarsh Kumar </p>
         </div>
       </div>
-
+      {/* Button with hovers  */}
       <button className=' bg-sky-500 mt-2 text-center text-white p-2 rounded-2xl hover:bg-white hover:text-black dark:to-blue-500'>Buy Now</button>
+
+      {/* Learning how sm and md working for creating a responsive design */}
         <div className='text-white sm:text-red-500 md:text-green-700'>
           <p className='text-center my-4'>Lorem inpsums dollor.</p>
         </div>
+ 
+
       </body>
     </>
   )
