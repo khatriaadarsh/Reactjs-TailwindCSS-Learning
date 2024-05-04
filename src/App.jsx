28fe2ded6af34803989befc373f1a95f7e04634b
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-04 21:19:55 +0500

    Align texts of card with hover and colors

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ const App = () => {
           <p className=' text-slate-500 text-base'>
           By Aadarsh Kumar 
         </p>
+        
        </div>
+        {/* Button with hovers  */}
+      <button className=' bg-sky-500 mt-2 text-center text-white p-2 rounded-2xl hover:bg-black hover:text-white  dark:to-blue-500'>Buy Now</button>
       </div>
-  {/* Button with hovers  */}
-      <button className=' bg-sky-500 mt-2 text-center text-white p-2 rounded-2xl hover:bg-white hover:text-black dark:to-blue-500'>Buy Now</button>
-
+ 
   {/* Learning how sm and md working for creating a responsive design */}
         <div className='text-white sm:text-red-500 md:text-green-700'>
           <p className='text-center my-4'>Lorem inpsums dollor.</p>
@@ -25,9 +26,19 @@ const App = () => {
              <div className=' max-w-sm mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl'>
                <div className='md:flex'>
                   <div> 
-                   <img className=' h-48 w-full object-cover md:h-full md:w-48' src="https://images.pexels.com/photos/2522657/pexels-photo-2522657.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                   <img className='h-48 w-full object-cover md:h-full md:w-48' src="https://images.pexels.com/photos/2522657/pexels-photo-2522657.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                   </div>
-                  <div>Contact</div>
+                  <div className='pt-8 pl-4 mb-4'>
+                    <div className='uppercase tracking-wide text-sm text-indigo-500 font-semibold'>
+                     An awesome card
+                    </div>
+                    <a className='pr-1 font-medium text-lg hover:text-blue-700' href="/">
+                        Tailwind CSS is amazing once easy to understand the power it.
+                    </a>
+                    <p className=' mt-2 text-slate-500'>
+                      Lorem ipsum dolor sit amet,
+                    </p>
+                  </div>
               </div>
             </div>
           </div>
